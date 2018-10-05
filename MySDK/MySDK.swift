007f29d42c2f7789a4b4f297e910d7fcecdff260
shindevivek68 <43874076+shindevivek68@users.noreply.github.com>
2018-10-05 16:14:56 +0530

    file added

---
+++ MySDK/MySDK.swift
@@ -0,0 +1,15 @@
+//
+//  MySDK.swift
+//  MySDK
+//
+//  Created by Vivek shinde on 05/10/18.
+//  Copyright © 2018 Vivek Shinde. All rights reserved.
+//
+
+import Foundation
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
