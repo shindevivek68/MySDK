4095c3372689693ace0e908c5cc87dd87004e4f1
shindevivek68 <43874076+shindevivek68@users.noreply.github.com>
2018-10-05 16:09:12 +0530

    file uploaded.

---
+++ MySDK.framework/Headers/MySDK.h
@@ -0,0 +1,19 @@
+//
+//  MySDK.h
+//  MySDK
+//
+//  Created by Vivek shinde on 05/10/18.
+//  Copyright © 2018 Vivek Shinde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySDK.
+FOUNDATION_EXPORT double MySDKVersionNumber;
+
+//! Project version string for MySDK.
+FOUNDATION_EXPORT const unsigned char MySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySDK/PublicHeader.h>
+
+
