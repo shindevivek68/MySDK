8d7343b24bcb7576326e1a29fee74852ee3ea5ea
shindevivek68 <43874076+shindevivek68@users.noreply.github.com>
2018-10-05 17:46:23 +0530

    new File added.

---
+++ MySDK/AttachmentUtility.swift
@@ -0,0 +1,15 @@
+//
+//  AttachmentUtility.swift
+//  Test
+//
+//  Created by Vivek shinde on 05/10/18.
+//  Copyright © 2018 Vivek Shinde. All rights reserved.
+//
+
+import UIKit
+
+public class AttachmentUtility: NSObject {
+    public func attachement(){
+        print("attachemnt added")
+    }
+}
